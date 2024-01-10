7556979d7d4b50b4e5054b3228baf4ee2e9a4560
Akram Ahmed Khan <akram02april@gmail.com>
2024-01-10 11:26:30 +0530

    hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,61 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends Component{
-	constructor() {
-		super()
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
+function App() {
+	// constructor() {
+	// 	super()
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchfield: ''
+	// 	}
+	// }
+	const [robots, setRobots] = useState([])
+	const [searchfield, setSearchfield] = useState('')
+	const [count, setCount] = useState(0);
 
-	componentDidMount() {
+	// componentDidMount() {
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 	.then(response =>  response.json())
+	// 	.then(users => this.setState({robots: users}));	
+	// }
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response =>  response.json())
-		.then(users => this.setState({robots: users}));	
-	}
+		.then(users => setRobots(users));	
+		// console.log(count)
+	}, []) //[{count}]) only run if count changes
 
     //this is not an inherent function thats why we have to use this arrow function
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value})
+	const onSearchChange = (event) => {
+		return setSearchfield(event.target.value)
 	}
-
-	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		// const {robots, searchfield} = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
-		if (this.state.robots.length === 0) {
+		
+		if (robots.length === 0) {
 			return <h1 className='tc'>Loading</h1>
 		}
 		else{
 		return (
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
+				{/* <button onClick={() => setCount(count + 1)}>Click Me!</button> */}
+				<SearchBox searchChange={onSearchChange}/>
 				<Scroll>
-					<ErrorBoundry>
-						<CardList robots={filteredRobots}/>
+					<ErrorBoundry> 
+						<CardList robots={filteredRobots}/> {/*children of scroll and error aka CardList props.children*/}
 					</ErrorBoundry>
 				</Scroll>
 			</div>
 		    );
 	    }
     }	
-}
+
 
 export default App;
